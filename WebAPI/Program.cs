1a7044fc30982a235a36b59776ff5a75bacb1a91
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-12 21:29:42 +0400

    updates for env variables

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,16 +1,22 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using WebAPI.Data;
-using WebAPI.Extensions;
 using WebAPI.Helpers;
 using WebAPI.Interfaces;
 using WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
+var builder2 = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("Default"));
+builder2.Password = builder.Configuration.GetSection("DBPassword").Value;
+var connectionString = builder2.ConnectionString;
 
 // Add services to the container.
+Host.CreateDefaultBuilder(args).ConfigureHostConfiguration(configHost => {
+        configHost.AddEnvironmentVariables(prefix: "HSPA_");
+});
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,7 +24,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+        options.UseSqlServer(connectionString));
 
 var secretKey = builder.Configuration.GetSection("AppSettings:Key").Value;
 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
