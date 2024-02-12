b498c15fce6f1fa3c8b2fb5ed96cf74286c4ebd7
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-13 01:34:59 +0400

    updatesss

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -14,9 +14,9 @@ builder2.Password = builder.Configuration.GetSection("DBPassword").Value;
 var connectionString = builder2.ConnectionString;
 
 // Add services to the container.
-Host.CreateDefaultBuilder(args).ConfigureHostConfiguration(configHost => {
-        configHost.AddEnvironmentVariables(prefix: "HSPA_");
-});
+// Host.CreateDefaultBuilder(args).ConfigureHostConfiguration(configHost => {
+//         configHost.AddEnvironmentVariables(prefix: "HSPA_");
+// });
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
