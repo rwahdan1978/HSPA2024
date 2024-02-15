cfd676df65b69949507753984c6188984fecd326
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-16 00:50:39 +0400

    update for all 16-2-2024

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5eb259a8f0e82b771808268d9690ccb68134261")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca36e68b87d31427d599a453f94f7bf022af8046")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
