4e7175ac516c64755bf810e68a7f1c44bf5bacee
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-17 14:23:12 +0400

    done error global

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf7314dc4f42c4dbe4110c474aedaf0412fcf37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053a519369b441342ea09f6c859e0ab470ac4dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
