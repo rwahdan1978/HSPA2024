7feae1c470cf62a00744aba3fec2ce41ed32127d
rwahdan1978 <86670955+rwahdan1978@users.noreply.github.com>
2024-02-25 13:32:46 +0400

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
     production: true,
     baseUrl: "https://hspa2024.azurewebsites.net/api",
-    
-    // uncomment the below code before building frontend
-    // AWS_ACCESS_KEY_ID: "AKIAT4QNA4ENP5XTNIHV",
-    // AWS_SECRET_ACCESS_KEY:  "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",
-    // AWS_DEFAULT_REGION: "ap-south-1",
-    // adminPass: "Fatima@2020"
 };
