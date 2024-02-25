d789e3f375903c5e31f8f510c414bc55a5f029bb
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-25 13:40:44 +0400

    updates

--- Frontend/src/app/upload/upload.service.ts
+++ Frontend/src/app/upload/upload.service.ts
@@ -10,6 +10,6 @@ constructor(private http: HttpClient) { }
 
 imageUpload(imageForm: FormData) {
   console.log('image uploading');
-  return this.http.post('https://nodejsapi-30tm.onrender.com/api/v1/upload/', imageForm);
+  return this.http.post('https://angular-upload-files-2023-2024.s3.amazonaws.com/api/v1/upload/', imageForm);
  }
 }
