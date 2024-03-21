23952964f4a2f39d65dba54619bacc261e7f47ba
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-03-22 01:34:26 +0400

    nav updates

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from '../services/alertify.service';
 import { Router } from '@angular/router';
 import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -13,10 +14,31 @@ export class NavBarComponent implements OnInit {
   deviveInfo: DeviceInfo;
   public today = Date.now();
   loggedinUser: string;
+  token:any;
 
   constructor(private alertify: AlertifyService, private router: Router, private DDS: DeviceDetectorService) { }
 
   ngOnInit() {
+
+    this.token = localStorage.getItem("token");
+
+    timer(0, 600000).subscribe(() => { 
+
+      const parseJwt = (this.token);        
+      const decode = JSON.parse(atob(this.token.split('.')[1]));
+      console.log(decode);
+      if (decode.exp * 1000 < new Date().getTime()) 
+      {
+        localStorage.removeItem('token');
+        localStorage.removeItem('chosenfolder');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('isAdmin');
+        localStorage.removeItem('userId');
+        this.router.navigate(["user/login"]);
+        this.alertify.error("Session Expired!")
+      }
+      
+    });
 
     this.deviveInfo = this.DDS.getDeviceInfo();
 
